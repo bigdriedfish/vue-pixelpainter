a999457828a868f388cf2ab05e27e30d85cfd222
xiaoyugan <1690439769@qq.com>
2020-03-27 14:22:40 +0800

    使用jimp传输数据并进行间歇绘制

--- src/app.js
+++ src/app.js
@@ -18,19 +18,28 @@ main()
 
 async function main() {
 	let img 
+	
+
 	try {
 		img = await Jimp.read(path.join(__dirname, './pixel.png'))
 	} catch (error) {
 		img = new Jimp(256, 256, 0xffffffff)
 	}
 	//缓存到本地
+	var lastUpdate = 0
 	setInterval(() => {
-		img.write(path.join(__dirname, './pixel.png'), ()=> {
-			console.log("data saved!")
-		})
+		var now = Date.now()
+		//间歇保存
+		if (now - lastUpdate < 3000) {
+			img.write(path.join(__dirname, './pixel.png'), ()=> {
+				console.log("data saved!", now)
+			})
+		}
+	
 	}, 3000)
 	
 	wss.on('connection',(ws, req) => {
+		//首次连接发送buffer数据
 		img.getBuffer(Jimp.MIME_PNG, (err, buf) => {
 			if(err) {
 				console.log('get buffer err', err)
@@ -45,15 +54,18 @@ async function main() {
 			msg = JSON.parse(msg)
 			var now = Date.now()
 			var {x, y, color} = msg
-			
-			//限制绘制次数
+			console.log('msg',msg)
+			//限制频繁绘制
 			if (msg.type == 'drawDot') {
 				if (now - lastDraw < 200) {
 					return 
 				}
 	
 				if (x > 0 && y > 0 && x < width && y < height) {
-					lastDraw = now 
+					lastDraw = now
+					//绘制更新间隔标志，用于判断是否保存到本地 
+					lastUpdate = now
+					//后端接收坐标颜色信息，并返回给前端
 					img.setPixelColor(Jimp.cssColorToHex(color), x, y)
 					wss.clients.forEach(client => {
 						client.send(JSON.stringify({
