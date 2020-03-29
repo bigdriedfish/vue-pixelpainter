2fc56e0f2dbe24262dd3944a79f555529b3bd97e
xiaoyugan <1690439769@qq.com>
2020-03-29 21:55:56 +0800

    使用socketio并实现批量更新用户操作

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ const path = require('path')
 const ws = require('ws')
 const express = require('express')
 const Jimp = require('jimp')
+const socketIO = require('socket.io')
 
 const port = 9095
 
 const app = express()
 const server = http.createServer(app)
-const wss = new ws.Server({server})
+const io = socketIO(server)
 
 const width = 256
 const height = 256
@@ -37,58 +38,52 @@ async function main() {
 		}
 	
 	}, 3000)
-	
-	wss.on('connection',(ws, req) => {
+	//批量更新
+	let userOperations = []
+
+	setInterval(() => {
+		io.emit('updateDot', userOperations)
+		userOperations = []
+	}, 300)
+
+	io.on('connection',(ws, req) => {
 		//首次连接发送buffer数据
 		img.getBuffer(Jimp.MIME_PNG, (err, buf) => {
 			if(err) {
 				console.log('get buffer err', err)
 			} else {
-				ws.send(buf)
+				ws.emit('init', buf)
 			}
 		})
 		//在线人数功能
-		wss.clients.forEach(ws => {
-			ws.send(JSON.stringify({
-				type:'onlineCount',
-				count:wss.clients.size,
-			}))
-		})
-
+		io.emit('onlineCount',{
+			count:Object.keys(io.sockets.sockets).length,
+		})		
 		ws.on('close', () => {
-			wss.clients.forEach(ws => {
-				ws.send(JSON.stringify({
-					type:'onlineCount',
-					count:wss.clients.size,
-				}))
+			io.emit('onlineCount',{
+				count:Object.keys(io.sockets.sockets).length,
 			})
 		})
 		var lastDraw = 0
 	
-		ws.on('message', msg => {
-			msg = JSON.parse(msg)
+		ws.on('drawDot',data => {
 			var now = Date.now()
-			var {x, y, color} = msg
-			//限制频繁绘制
-			if (msg.type == 'drawDot') {
-				if (now - lastDraw < 200) {
-					return 
-				}
-	
-				if (x > 0 && y > 0 && x < width && y < height) {
-					lastDraw = now
-					//绘制更新间隔标志，用于判断是否保存到本地 
-					lastUpdate = now
-					//后端接收坐标颜色信息，并返回给前端
-					img.setPixelColor(Jimp.cssColorToHex(color), x, y)
-					wss.clients.forEach(client => {
-						client.send(JSON.stringify({
-							type: 'updateDot',
-							x, y, color
-						}))
-					})
-				}
+			var {x, y, color} = data
+			if (now - lastDraw < 200) {
+				return 
 			}
+
+			if (x > 0 && y > 0 && x < width && y < height) {
+				lastDraw = now
+				//绘制更新间隔标志，用于判断是否保存到本地 
+				lastUpdate = now
+				//后端接收坐标颜色信息，并返回给前端
+				img.setPixelColor(Jimp.cssColorToHex(color), x, y)
+				// io.emit('updateDot', {
+				// 	x, y, color
+				// })
+				userOperations.push({x, y, color})
+			}	
 		})
 	})
 	
